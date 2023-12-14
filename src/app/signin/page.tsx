b167957492e007f6178efdee4796446d3397a77a
DaisukeMiyauchi-nippon <miyauchi.kimimaro@gmail.com>
2023-12-15 00:28:42 +0900

    サインアップ画面の見栄えはほぼOK

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,64 +1,93 @@
-"use client"
-import Head from "next/head";
+"use client";
 import { supabase } from "@/utils/supabaseClient";
 import { useState } from "react";
-import Link from 'next/link';
-import { useRouter } from 'next/navigation'
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
-export default function Login(){
-    const router = useRouter()
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+export default function Login() {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const onLogin = async() => {
-      const { data,error } = await supabase.auth.signInWithPassword({
-        email: email,
-        password: password,
-      })}
+  const onLogin = async () => {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
+  };
 
   return (
-    <div>
-      <Head>
-        <title>ログイン画面</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <div>
-          <form>
-          <div>
-            <label>メールアドレス</label>
-            <input type="email"
-              required value={email}
-              onChange={e => setEmail(e.target.value)}
-            />
-          </div>
-          <div>
-            <label>パスワード</label>
-            <input type="password"
-              required value={password}
-              onChange={e => setPassword(e.target.value)}
-            />
-          </div>
-          <div>
-          <button
-                style={{ width: 220 }}
-                color="primary"
-                // 登録ボタンがクリックされたとき関数が実行されるようにする
-                onClick={()=>{onLogin();
-                }}
-              >ログイン</button>
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          ログイン
+        </h2>
+      </div>
 
-            <Link href='/signup'>
-              ユーザー登録がお済みでない方はこちらから
-            </Link><br/>
-            <Link href='/sendemail'>
-              パスワードをお忘れの方はこちらから
-            </Link>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6">
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              メールアドレス
+            </label>
+            <div className="mt-2">
+              <input
+                type="email"
+                autoComplete="email"
+                required value={email}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                パスワード
+              </label>
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-semibold text-indigo-600 hover:text-indigo-500"
+                >
+                  パスワードをお忘れですか？
+                </a>
+              </div>
+            </div>
+            <div className="mt-2">
+              <input
+                type="password"
+                autoComplete="current-password"
+                required value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              // 登録ボタンがクリックされたとき関数が実行されるようにする
+              onClick={() => {
+                onLogin();
+              }}
+            >
+              ログイン
+            </button>
+
+            <Link href="/signup">ユーザー登録がお済みでない方はこちらから</Link>
           </div>
         </form>
-        </div>
-      </main>
+      </div>
     </div>
-  )
+  );
 }
-
